604b0d3312ee5ec8d5cd144cacff8a8f87428565
TomEnChih <tom10485916@gmail.com>
2023-06-02 18:05:46 +0800

    Chapter 11 - 1

---
+++ valsrevenge/Components/HealthComponent.swift
@@ -0,0 +1,32 @@
+//
+//  HealthComponent.swift
+//  valsrevenge
+//
+//  Created by user on 2023/6/1.
+//
+
+import UIKit
+import GameplayKit
+
+class HealthComponent: GKComponent {
+    
+    override func didAddToEntity() {
+        
+    }
+    
+    func setupBar(at num: Int, tint: SKColor? = nil) {
+        
+    }
+    
+//    override func willRemoveFromEntity() {
+//
+//    }
+//
+//    override func update(deltaTime seconds: TimeInterval) {
+//
+//    }
+    
+    override class var supportsSecureCoding: Bool {
+        true
+    }
+}
